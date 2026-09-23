67b6dfb76c6fbf589f778d7315f7ac63650cfece
naiful_naife <naifulnaife009@gmail.com>
2026-09-23 21:51:04 +0600

    Project Setup

--- src/app/my-plan/page.tsx
+++ src/app/my-plan/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      Homepage
-    </div>
-  );
+    return (
+        <div>
+            My plans
+        </div>
+    );
 };
 
 export default page;
